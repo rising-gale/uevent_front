41dabed4c66ec41f5e925bc9320b8e1ecb370524
Mint_Caramel <91499156+LizaNilova@users.noreply.github.com>
2023-03-26 17:46:28 +0300

    Add colors to tailwind.config.js
    I added color palette from this link:
    
    https://coolors.co/palette/22223b-4a4e69-9a8c98-c9ada7-f2e9e4

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,13 @@ module.exports = {
         '2screen':'200vh'
       }
     },
+    colors:{
+      'dark-purple':'#22223B',
+      'dark-blue-pastel': '#4A4E69',
+      'light-grey-pastel': '#9ABC98',
+      'beige' : 'C9ADA7',
+      'light-beige': '#F2E9E4',
+    },
   },
   plugins: [],
 }
