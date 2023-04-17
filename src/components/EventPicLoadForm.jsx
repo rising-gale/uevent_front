596b0e3caa33103f8e8605d0eb10ed75654ccc0c
Maksym Rekalo <maksim.monor@gmail.com>
2023-04-17 22:28:23 +0200

    upd

--- src/components/EventPicLoadForm.jsx
+++ src/components/EventPicLoadForm.jsx
@@ -21,7 +21,7 @@ const EventPicLoadForm = ({ data, closeForm }) => {
     }
 
     return (
-        <div className="text-white justify-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-slate-600 bg-opacity-50">
+        <div className="text-white items-center justify-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-slate-600 bg-opacity-50">
             <div className="relative my-2 mx-auto w-1/2">
                 {/*content*/}
                 <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-dark-blue-pastel outline-none focus:outline-none">
